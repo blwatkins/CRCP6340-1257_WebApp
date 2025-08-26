d34dc20bb30552fa5ca713decebc3313d8ee86b4
blwatkins <blwatkins@users.noreply.github.com>
2025-08-26 17:15:46 -0500

    Remove unneeded route from app.js

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const port = 3000;
 
 app.use(express.static('public'));
 
-app.get('/', (request, response) => {
-    response.send('Hello, World!');
-});
-
 app.listen(port, () => {
-    console.log(`Hello World app listening at http://localhost:${port}`);
+    console.log(`CRCP 6340 (1257) WebApp listening at http://localhost:${port}`);
 });
