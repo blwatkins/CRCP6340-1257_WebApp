ef0b354511ac7d3f0c53a935bc036a3b1393f25c
blwatkins <blwatkins@users.noreply.github.com>
2025-08-29 14:20:52 -0500

    Utilize validation and sanitization methods to make sure emails are not sent with empty subject or body.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-import { sendContactEmail } from './utils/utils.js';
+import { isValidString, sanitizeString, sendContactEmail } from './utils/utils.js';
 
 dotenv.config();
 
@@ -12,20 +12,35 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.post('/mail', async (request, response) => {
-    console.log('mail request received');
+    console.debug('Mail request received.');
 
-    // TODO - validate request body
-    // TODO - sanitize request body
-    // TODO - set proper subject and text from request body
-    await sendContactEmail('[TEST]: Test Subject', '[TEST]: This is a test email body.')
-        .then(() => {
-            console.log('Email sent successfully from app.js');
-            response.send('Email sent successfully from app.js');
-        })
-        .catch((error) => {
-            console.error(`Error sending email from app.js: ${error}`);
-            response.status(500).send(`Error sending email from app.js: ${error}`);
-        });
+    const requestSubject = request.body.subject;
+    const requestMessage = request.body.message;
+    let subject;
+    let message;
+
+    if (isValidString(requestSubject)) {
+        subject = sanitizeString(requestSubject);
+    }
+
+    if (isValidString(requestMessage)) {
+        message = sanitizeString(requestMessage);
+    }
+
+    if (subject && message) {
+        await sendContactEmail(subject, message)
+            .then(() => {
+                const successMessage = 'Email sent successfully.';
+                console.debug(successMessage);
+                response.send(successMessage);
+            })
+            .catch((error) => {
+                console.error(`Error sending email: ${error}`);
+                response.status(500).send(`Error sending email.`);
+            });
+    } else {
+        response.status(400).send('Invalid request format.');
+    }
 });
 
 app.listen(port, () => {
