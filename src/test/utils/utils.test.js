da13f8f616e4f7b0f1fd1423e02e4fec7184e1ca
blwatkins <blwatkins@users.noreply.github.com>
2025-08-29 17:12:50 -0500

    #18 Add initial tests for isValidString method.

--- src/test/utils/utils.test.js
+++ src/test/utils/utils.test.js
@@ -1,1 +1,27 @@
-test.todo('Write tests for utils');
+import { isValidString } from '../../main/utils/utils.js';
+
+describe('utils.js', () => {
+    describe('isValidString()', () => {
+        test.each([
+            { input: 'value', expected: true },
+            { input: '', expected: false },
+            { input: '        ', expected: false },
+            { input: '\n', expected: false },
+            { input: '\t', expected: false },
+            { input: '\n\t', expected: false },
+            { input: null, expected: false },
+            { input: undefined, expected: false },
+            { input: 123, expected: false },
+            { input: {}, expected: false },
+            { input: [], expected: false },
+            {
+                input: () => {
+                    return 'test';
+                },
+                expected: false
+            }
+        ])('isValidString($input)', ({ input, expected }) => {
+            expect(isValidString(input)).toBe(expected);
+        });
+    });
+});
