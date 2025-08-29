e0c37c08abdc92b02a6b9bbf0fb7771127098ab0
blwatkins <blwatkins@users.noreply.github.com>
2025-08-29 10:56:35 -0500

    Add initial nodemailer logic.

---
+++ utils/utils.js
@@ -0,0 +1,40 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+function sendEmail(subject, text) {
+    if (verifyMailSettings()) {
+        const transport = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: process.env.SMTP_SECURE,
+            requireTLS: process.env.SMTP_REQUIRE_TLS,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASSWORD
+            }
+        });
+
+        const message = {
+            from: process.env.MAIL_FROM,
+            to: process.env.MAIL_TO,
+            subject: subject,
+            text: text
+        };
+    } else {
+        throw new Error('Mail settings are not properly configured.');
+    }
+}
+
+function verifyMailSettings() {
+    const host = process.env.SMTP_HOST;
+    const port = process.env.SMTP_PORT;
+    const secure = process.env.SMTP_SECURE;
+    const requireTLS = process.env.SMTP_REQUIRE_TLS;
+    const user = process.env.MAIL_USER;
+    const password = process.env.MAIL_PASSWORD;
+    const mailFrom = process.env.MAIL_FROM;
+    const mailTo = process.env.MAIL_TO;
+    return host && port && secure && requireTLS && user && password && mailFrom && mailTo;
+}
