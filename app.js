64427d1853a9c463b28e01179fd6e88441d9e80b
blwatkins <blwatkins@users.noreply.github.com>
2025-08-29 09:57:27 -0500

    Add dotenv and express.json to app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.use(express.static('public'));
 
 app.post('/mail', (request, response) => {
