c390aae6e071e0fcde863658887a215136e80c23
blwatkins <blwatkins@users.noreply.github.com>
2025-08-29 16:48:20 -0500

    #17 #18 Add initial jest configuration and script

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+const config = {
+    clearMocks: true,
+
+    collectCoverage: true,
+    coverageDirectory: './out/tests-coverage',
+    coverageReporters: ['text', 'lcov', 'json', 'json-summary', 'clover'],
+    coverageProvider: 'v8',
+
+    errorOnDeprecated: true,
+
+    verbose: true
+};
+
+export default config;
