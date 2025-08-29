4fd9f904d206e0ca914fd65ea6f68ce276b8df4c
blwatkins <blwatkins@users.noreply.github.com>
2025-08-28 22:04:25 -0500

    Add /mail POST route to app.js

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const port = 3000;
 
 app.use(express.static('public'));
 
+app.post('/mail', (request, response) => {
+    console.log('mail request received');
+    response.send('mail request received');
+});
+
 app.listen(port, () => {
     console.log(`CRCP 6340 (1257) WebApp listening at http://localhost:${port}`);
 });
