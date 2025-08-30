29835733255ef593e23cedc81dd664233e3d2907
blwatkins <blwatkins@users.noreply.github.com>
2025-08-29 19:30:56 -0500

    #17 Add unit tests for POST /mail route.

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,1 +1,86 @@
-test.todo('Write tests for app.js');
+import request from 'supertest';
+
+import { afterAll, afterEach, jest } from '@jest/globals';
+
+jest.unstable_mockModule('nodemailer', () => ({
+    createTransport: jest.fn()
+}));
+
+let nodemailer;
+
+let app;
+
+beforeAll(async () => {
+    app = (await import('../main/app.js')).app;
+    nodemailer = await import('nodemailer');
+});
+
+afterAll(async () => {
+    jest.clearAllMocks();
+    jest.resetModules();
+});
+
+const ORIGINAL_ENV = process.env;
+
+const TEST_ENV = {
+    ...ORIGINAL_ENV,
+    SMTP_SERVICE: 'fake',
+    SMTP_REQUIRE_TLS: 'true',
+    MAIL_USER: 'user@fake-website.fake',
+    MAIL_PASSWORD: 'password',
+    MAIL_FROM: 'from@fake-website.fake',
+    MAIL_TO: 'to@fake-website.fake'
+};
+
+describe('POST /mail', () => {
+    beforeEach(() => {
+        process.env = { ...ORIGINAL_ENV };
+        nodemailer.createTransport.mockClear();
+    });
+
+    afterEach(() => {
+        process.env = ORIGINAL_ENV;
+    });
+
+    test('POST /mail - success', async () => {
+        const sendMailMock = jest.fn().mockResolvedValue('success');
+        nodemailer.createTransport.mockReturnValue({ sendMail: sendMailMock });
+
+        process.env = { ...TEST_ENV };
+
+        const response = await request(app)
+            .post('/mail')
+            .send({ subject: 'Test Subject', message: 'Test Body' });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe('Email sent successfully.');
+        expect(sendMailMock).toHaveBeenCalledWith({
+            from: TEST_ENV.MAIL_FROM,
+            to: TEST_ENV.MAIL_TO,
+            subject: 'Test Subject',
+            text: 'Test Body'
+        });
+    });
+
+    test('should respond with 400 if subject or message is invalid', async () => {
+        const response = await request(app)
+            .post('/mail')
+            .send({ subject: '', message: '' });
+
+        expect(response.statusCode).toBe(400);
+        expect(response.text).toBe('Invalid request format.');
+    });
+
+    test('should respond with 500 if sendEmail throws error', async () => {
+        const sendMailMock = jest.fn().mockRejectedValue(new Error('SMTP error'));
+        nodemailer.createTransport.mockReturnValue({ sendMail: sendMailMock });
+        process.env = { ...TEST_ENV };
+
+        const response = await request(app)
+            .post('/mail')
+            .send({ subject: 'Test Subject', message: 'Test Body' });
+
+        expect(response.statusCode).toBe(500);
+        expect(response.text).toBe('Error sending email.');
+    });
+});
