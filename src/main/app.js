a31a0a0fb4790eaae1ff84c4cb0785a996d55d3a
blwatkins <blwatkins@users.noreply.github.com>
2025-08-29 19:59:03 -0500

    Add check for parsed request body.

--- src/main/app.js
+++ src/main/app.js
@@ -33,30 +33,34 @@ app.use(express.static('public'));
 app.post('/mail', async (request, response) => {
     console.debug('Mail request received.');
 
-    const requestSubject = request.body.subject;
-    const requestMessage = request.body.message;
-    let subject;
-    let message;
+    if (request.body) {
+        const requestSubject = request.body.subject;
+        const requestMessage = request.body.message;
+        let subject;
+        let message;
 
-    if (isValidString(requestSubject)) {
-        subject = sanitizeString(requestSubject);
-    }
+        if (isValidString(requestSubject)) {
+            subject = sanitizeString(requestSubject);
+        }
 
-    if (isValidString(requestMessage)) {
-        message = sanitizeString(requestMessage);
-    }
+        if (isValidString(requestMessage)) {
+            message = sanitizeString(requestMessage);
+        }
 
-    if (subject && message) {
-        await sendEmail(subject, message)
-            .then(() => {
-                const successMessage = 'Email sent successfully.';
-                console.debug(successMessage);
-                response.send(successMessage);
-            })
-            .catch((error) => {
-                console.error(`Error sending email: ${error}`);
-                response.status(500).send(`Error sending email.`);
-            });
+        if (subject && message) {
+            await sendEmail(subject, message)
+                .then(() => {
+                    const successMessage = 'Email sent successfully.';
+                    console.debug(successMessage);
+                    response.send(successMessage);
+                })
+                .catch((error) => {
+                    console.error(`Error sending email: ${error}`);
+                    response.status(500).send(`Error sending email.`);
+                });
+        } else {
+            response.status(400).send('Invalid request format.');
+        }
     } else {
         response.status(400).send('Invalid request format.');
     }
